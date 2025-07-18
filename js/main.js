390119becf0071aebb1ed3bbf22eba4b2b49de5f
aryankandpal04 <aryankandpal04@gmail.com>
2025-07-18 16:01:16 +0530

    Implement Typed.js functionality for dynamic text display and update styles for the typed text element in index.html. Move Typed.js initialization to main.js for better organization.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+  // Typed.js initialization
+  if (window.Typed) {
+    new Typed('#typed-text', {
+      strings: ['1st–12th Classes', 'BCA, BSc, BBA, B.Pharma', 'Sainik School Exam Preparation',' Navodaya Vidyala Exam Prepration', 'Hindi and English Medium','CBSE and UK Boards','Mathematics','Science','Economics','Chemistry','Physics','All Subjects'],
+      typeSpeed: 55, backSpeed: 30, loop: true
+    });
+  }
